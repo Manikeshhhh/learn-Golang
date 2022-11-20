132f03924fd4856aeb2efc97bb56b01d3985a2ba
Manikeshhhh <manikesh182003@gmail.com>
2022-11-20 18:58:50 +0530

    added pointers

--- 6. memorymanagement/main.go
+++ 6. memorymanagement/main.go
@@ -1,4 +1,8 @@
-//new() -  allocate memoryy but no init,you will get memory address,zerored memory
+//new() -  allocate memoryy but no init,you will get memory address,zerored memoryyyy
 //make() - allocate and init, you will get a memory address, non zerored memoryyy
+//garbage collections happens automatically
+
+
+
 
 
---
+++ 7. Pointers/main.go
@@ -0,0 +1,17 @@
+//pointers gives us surety that whatever we are passing on as a pointer is infact the same variable and not the copy of it
+//We pass memory address for pointers
+//Pointer is just a direct reference to memory address of the variable 
+package main
+import "fmt"
+func main(){
+	fmt.Println("Welcome to pointersssss")
+	// var point *int
+	// fmt.Println("Default value of pointer is: ", point)
+	mynumber := 245
+	var point = &mynumber  //& is referencing a pointer
+	fmt.Println("address of  pointers is: ", point)
+	fmt.Println("address of  pointers is: ", *point)  //This is value inside pointer
+
+	*point = *point * 2  
+	fmt.Println("Multiplied value is: ", mynumber)
+}
