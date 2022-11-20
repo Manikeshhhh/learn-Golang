350ec705532b0fa9109103a2465d770723a07bd1
Manikeshhhh <manikesh182003@gmail.com>
2022-11-20 18:15:51 +0530

    Fix bad repo

--- 6. memorymanagement/main.go
+++ 6. memorymanagement/main.go
@@ -1,3 +1,4 @@
 //new() -  allocate memoryy but no init,you will get memory address,zerored memory
-//make() - allocate and init, you will get a memory address, non zerored memory 
+//make() - allocate and init, you will get a memory address, non zerored memoryyy
+
 
