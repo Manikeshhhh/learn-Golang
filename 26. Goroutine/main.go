7eb818267e07f12b48ea9d1b3be6cdba8430cab2
Manikeshhhh <manikesh182003@gmail.com>
2022-12-09 20:35:12 +0530

    mutex

--- 26. Goroutine/main.go
+++ 26. Goroutine/main.go
@@ -1,17 +1,56 @@
 package main 
 import "fmt"
-import "time"
+import "net/http"
+import "log"
+import "sync"
+/*import "time"*/
+
+var wg sync.WaitGroup //pointer
+
+var signals =  []string{"test"}
+var mut sync.Mutex //pointer
+
+
+
 func main(){
 	fmt.Println("Why are you here")
-	go greeter("Hello")
-	greeter("World")
+/*	go greeter("Hello")
+	greeter("World")*/
 
-}
+	websitelist:=[]string{
+		"https://manikeshhhh.github.io",
+		"https://lco.dev",
+		"https://google.com",
+		"https://fb.com",
+		"https://github.com",
+	}
+	for _,web:=range websitelist{
+		go getStatusCode(web)
+		wg.Add(1)
+	}
+	wg.Wait()
+	fmt.Println(signals)
 
-func greeter(s string){
+}	
+
+/*func greeter(s string){s
 
 	for i:=0;i<10;i++{
 		time.Sleep(5*time.Millisecond)
 		fmt.Println(s)
 	}
+}
+*/
+
+func getStatusCode(endpoint  string){
+	defer wg.Done()
+	res,err:=http.Get(endpoint)
+	if err != nil {
+		log.Fatal(err) 
+	}else{
+	mut.Lock()
+	signals = append(signals,endpoint)
+	mut.Unlock()
+	fmt.Printf("%d Status code for the %s\n",res.StatusCode,endpoint)
+}
 }
